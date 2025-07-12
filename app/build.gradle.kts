8f240c157860941771f0e59232e9ed594398cb51
ThisaraniWickramasinghe <thisaranidhammamali@gmail.com>
2025-07-13 00:42:34 +0530

    Created Scan QR, and Teacher and Student dashboards

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,5 +60,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //QR scan
+    implementation("com.journeyapps:zxing-android-embedded:4.1.0")
 }
 
